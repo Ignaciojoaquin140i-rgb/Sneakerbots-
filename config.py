dccd4d8852bff9cd005d27e3a9d9310626b985a5
Ignaciojoaquin140i-rgb <ignaciojoaquin140i@gmail.com>
2026-04-01 18:17:19 -0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,46 @@
+config.py
+TELEGRAM_TOKEN = "PEGA_TU_TOKEN_AQUI"
+CHAT_ID = "PEGA_TU_CHAT_ID_AQUI"
+INTERVALO_MINUTOS = 30
+EBAY_MAX_PRECIO_USD = 100
+EBAY_BUSQUEDAS = [
+    "Air Jordan 1 Retro OG Patent Bred",
+    "Air Jordan 3 Retro OG Fire Red",
+    "Air Jordan 3 Retro OG Racer Blue",
+    "Air Jordan 3 Retro OG Black Cement",
+    "Air Jordan 3 Retro OG White Cement",
+    "Air Jordan 3 Retro OG UNC",
+    "Air Jordan 3 Retro OG Tinker Cement",
+    "Air Jordan 3 Retro OG Tinker Black Cement",
+    "Air Jordan 3 Retro OG Black Cat",
+    "Air Jordan 4 Retro OG Bred",
+    "Air Jordan 4 Retro OG Fire Red",
+    "Air Jordan 4 Retro OG Black Cat",
+    "Air Jordan 4 Retro OG White Cement",
+    "Air Jordan 4 Retro OG Motorsport",
+    "Air Jordan 4 Retro OG Oreo Black",
+    "Air Jordan 5 Retro OG Black Metallic",
+    "Air Jordan 5 Retro OG Fire Red",
+    "Air Jordan 6 Retro OG Reverse Bred",
+    "Air Jordan 6 Retro OG Infrared",
+    "Air Jordan 6 Retro OG DMP",
+    "Air Jordan 8 Retro OG Aqua",
+    "Air Jordan 8 Retro OG Playoff",
+    "Air Jordan 11 Retro OG Bred",
+    "Air Jordan 11 Retro OG Concord Bred",
+    "Air Jordan 11 Retro OG Space Jam",
+    "Air Jordan 11 Retro OG Concord",
+    "Air Jordan 11 Retro OG DMP",
+    "Air Jordan 13 Retro OG Black Cat",
+    "Air Jordan 13 Retro OG Flint",
+    "Air Jordan 13 Retro OG Cap and Gown",
+]
+DESCUENTO_MINIMO_CHILE = 40
+CATEGORIAS_CHILE = ["celulares", "notebooks", "televisores", "tablets", "pc"]
+TIENDAS_CHILE = {
+    "Falabella": {"base_url": "https://www.falabella.com/falabella-cl", "activa": True},
+    "Ripley": {"base_url": "https://simple.ripley.cl", "activa": True},
+    "Paris": {"base_url": "https://www.paris.cl", "activa": True},
+    "PC Factory": {"base_url": "https://www.pcfactory.cl", "activa": True},
+    "AbcDin": {"base_url": "https://www.abcdin.cl", "activa": True},
+}
